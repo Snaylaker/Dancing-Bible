cb890a64db99ec6472e1dd5e5c705bcbf729660c
pavlocha <mehdihimmid@hotmail.com>
2022-12-08 20:21:01 +0100

    added chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
